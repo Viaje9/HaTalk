ade3e99a63efc206a1ea3b6c3c541bf571f8529b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-17 12:48:37 +0000

    Deploy hatalk.viaje9.com to github.com/Viaje9/HaTalk.git:gh-pages

---
+++ precache-manifest.fb82b6d654f021f2c8d0f91b8226d72c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab193e8d0eba4a5c94",
+    "url": "/css/app.c8133773.css"
+  },
+  {
+    "revision": "097bbef10ded37533936",
+    "url": "/css/chunk-vendors.44220183.css"
+  },
+  {
+    "revision": "fb9d5e1d61f8d875b9e092285a1bfc33",
+    "url": "/img/account.fb9d5e1d.svg"
+  },
+  {
+    "revision": "8209f28520e30407a726cb4209fab45f",
+    "url": "/img/avatar.8209f285.svg"
+  },
+  {
+    "revision": "37660171a0e69ee6d980c78a5094f202",
+    "url": "/img/lock.37660171.svg"
+  },
+  {
+    "revision": "20bb51240f340e82f1db6c425aa2bca0",
+    "url": "/img/name.20bb5124.svg"
+  },
+  {
+    "revision": "ce0409d280667e157f62949c317b4295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ab193e8d0eba4a5c94",
+    "url": "/js/app.7bd0a432.js"
+  },
+  {
+    "revision": "097bbef10ded37533936",
+    "url": "/js/chunk-vendors.403be518.js"
+  },
+  {
+    "revision": "9da1f908ab1b1d744e1266afe5021564",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
